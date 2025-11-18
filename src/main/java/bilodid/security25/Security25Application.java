f2b77079c0cd22b4bd12c29a127b49fa0390054f
машуля <belonglove638@gmail.com>
2025-11-18 20:55:33 +0200

    Initial commit

---
+++ src/main/java/bilodid/security25/Security25Application.java
@@ -0,0 +1,13 @@
+package bilodid.security25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security25Application.class, args);
+	}
+
+}
---
+++ src/test/java/bilodid/security25/Security25ApplicationTests.java
@@ -0,0 +1,13 @@
+package bilodid.security25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
