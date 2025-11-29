4092a79f59d22eb13840066a21b90b30cebb5910
машуля <belonglove638@gmail.com>
2025-11-30 00:15:20 +0200

    Annotationns

--- src/main/java/bilodid/security25/config/SecurityConfig.java
+++ src/main/java/bilodid/security25/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package bilodid.security25.config;
 
+import org.springframework.aop.Advisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.method.AuthorizationManagerBeforeMethodInterceptor;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -21,8 +24,13 @@ import org.springframework.security.config.Customizer;
 */
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 
 public class SecurityConfig {
+
+    public static Advisor preAuthorizeMethodInterceptor(){
+        return AuthorizationManagerBeforeMethodInterceptor.preAuthorize();
+    };
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
@@ -35,8 +43,8 @@ public class SecurityConfig {
         http.csrf(csrf ->csrf.disable())
                 .authorizeHttpRequests( req ->
                         req.requestMatchers("/index.html").permitAll()
-                                .requestMatchers("/api/v1/kittens/admin").hasRole("ADMIN")
-                                .requestMatchers("/api/v1/kittens/user").hasAnyRole("USER","ADMIN")
+                                //.requestMatchers("/api/v1/kittens/admin").hasRole("ADMIN")
+                                //.requestMatchers("/api/v1/kittens/user").hasAnyRole("USER","ADMIN")
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
 
--- src/main/java/bilodid/security25/item/KittenRestController.java
+++ src/main/java/bilodid/security25/item/KittenRestController.java
@@ -1,6 +1,7 @@
 package bilodid.security25.item;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,5 +36,32 @@ public class KittenRestController {
     @PutMapping
     public Kitten update(@RequestBody Kitten kitten) {
         return service.update(kitten);
+    }
+    @GetMapping("/hello/user")
+    @PreAuthorize("hasAnyRole('USER')")
+    public String helloUser() {
+        return "Hello User!";
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    @GetMapping("hello/admin")
+    public String helloAdmin() {
+        return "Hello Admin!";
+    }
+
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    @GetMapping("hello/unknown")
+    public String helloUnknown() {
+        return "Hello Unknown!";
+    }
+
+    @GetMapping("hello/stranger")
+    public String helloStranger() {
+        return "Hello Stranger!";
+    }
+    @PreAuthorize("hasRole('SUPERADMIN')")
+    @GetMapping("hello/superadmin")
+    public String helloSuperAdmin() {
+        return "Hello SuperAdmin!";
     }
 }
